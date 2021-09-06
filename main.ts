d1119152c233ea7240a5c1cb1dde0fca75fbf6c0
Morten Skogly <morten.skogly@gmail.com>
2021-09-06 14:46:40 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,6 @@ input.onButtonPressed(Button.A, function () {
     servos.P0.setAngle(0)
 })
 input.onButtonPressed(Button.B, function () {
-    servos.P0.setAngle(90)
+    servos.P0.setAngle(45)
 })
 servos.P0.setAngle(90)
-basic.forever(function () {
-	
-})

dadbb00fb4f8b88a502f0213e24c449e79ac8ac9
Morten Skogly <morten.skogly@gmail.com>
2021-09-06 14:54:38 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,9 @@ input.onButtonPressed(Button.B, function () {
     servos.P0.setAngle(45)
 })
 servos.P0.setAngle(90)
+loops.everyInterval(randint(3000, 60000), function () {
+    servos.P0.setAngle(0)
+    basic.pause(500)
+    servos.P0.setAngle(45)
+    basic.pause(500)
+})
